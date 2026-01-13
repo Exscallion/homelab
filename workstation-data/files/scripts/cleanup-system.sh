de97c5242d4ddd5cdea60f0ff11aa2eb2e66c4da
DrSkitterbug <176167962+DrSkitterbug@users.noreply.github.com>
2026-01-13 11:28:35 +0100

    fix(wsd): Fixes to make the systemd service work (i'm dumb)

--- workstation-data/files/scripts/cleanup-system.sh
+++ workstation-data/files/scripts/cleanup-system.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-set -euo pipefail
 
+echo "==> Removing generations..."
 /run/current-system/sw/bin/nix-env --profile /nix/var/nix/profiles/system --delete-generations +5
+
+echo "==> Collecting garbage..."
 /run/current-system/sw/bin/nix-store --gc
+
+echo "==> Optimizing nix store..."
 /run/current-system/sw/bin/nix-store --optimise
+
+echo "Happy days, enjoy your clean system!!"
